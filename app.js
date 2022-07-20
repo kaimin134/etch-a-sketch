e3b1cbb50dadecd4da4af5cd52c2e01a9c39d547
Minh Chung <chungvungocminh@gmail.com>
2022-07-20 00:51:20 -0500

    rainbow

--- app.js
+++ app.js
@@ -2,7 +2,24 @@
 const container = document.querySelector("div");
 for (i = 0; i < 256; i++) {
     let square = document.createElement('div');
-    square.classList.add("grid");
+    square.setAttribute("id", "grid");
     container.appendChild(square);
 }
-console.log("Hello");
+
+const grids = document.querySelectorAll("#grid");
+grids.forEach((grid) => {
+    grid.addEventListener('mouseover', () => {
+        grid.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`;
+    });
+    grid.addEventListener("mouseout", () => {
+        grid.style.backgroundColor = 'bisque';
+    });
+
+});
+
+function random() {
+    return Math.floor(Math.random() * 256);
+}
+
+console.log(random());
+
