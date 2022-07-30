312b213262639a75ac680223efee381edc7ab0a0
Minh Chung <chungvungocminh@gmail.com>
2022-07-30 16:15:42 -0500

    finish

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 
 const container = document.querySelector("div");
-for (i = 0; i < 256; i++) {
+const button = document.querySelector("button")
+let num = 66;
+function createGrid (x) {
+
+for (i = 0; i < x * x; i++) {
     let square = document.createElement('div');
     square.setAttribute("id", "grid");
+    square.style.height = `${1000 / x}px`;
+    square.style.width = `${1000 / x}px`;
     container.appendChild(square);
 }
+}
+createGrid(66);
+
+function removeGrid (x) {
+
+    for (i = 0; i < x * x; i++) {
+  
+        container.removeChild(container.lastElementChild);
+    }
+};
+
+function setSquare () {
+    removeGrid(num);
+    num = prompt("How many squares per side? (Enter from 1 - 100)");
+    console.log("Hello");
+    createGrid(num);
+}
+
+button.addEventListener('click', setSquare);
+
 
 const grids = document.querySelectorAll("#grid");
 grids.forEach((grid) => {
     grid.addEventListener('mouseover', () => {
         grid.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`;
     });
-    grid.addEventListener("mouseout", () => {
-        grid.style.backgroundColor = 'bisque';
-    });
-
 });
 
 function random() {
